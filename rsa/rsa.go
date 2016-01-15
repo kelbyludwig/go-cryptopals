4be96ac70fa8f69a1fad670614f6e0f0afbb2b26
Kelby Ludwig <kelbyludwig@gmail.com>
2016-01-14 22:03:32 -0600

    Fixed RSA enc and dec

--- rsa/rsa.go
+++ rsa/rsa.go
@@ -2,6 +2,7 @@ package rsa
 
 import (
 	"crypto/rand"
+	"crypto/rsa"
 	"errors"
 	"math/big"
 )
@@ -21,26 +22,9 @@ type KeyPair struct {
 	PrivateKey *PrivateKey
 }
 
-//GenerateBigPrime generates a *big.Int probable prime of parameter "bytes" size.
-func GenerateBigPrime(bytes uint) (p *big.Int, err error) {
-	p = new(big.Int)
-	bytesBuffer := make([]byte, bytes)
-	for {
-		_, err = rand.Read(bytesBuffer)
-		if err != nil {
-			return
-		}
-		p.SetBytes(bytesBuffer)
-		if p.ProbablyPrime(20) {
-			err = nil
-			return
-		}
-	}
-}
-
 //GenerateKeyPair generates an RSA public/private keypair of size bits
 //from a CSPRNG.
-func GenerateKeyPair(bits uint) (keypair *KeyPair, err error) {
+func GenerateKeyPair(bits int) (keypair *KeyPair, err error) {
 	keypair = new(KeyPair)
 	keypair.PublicKey = new(PublicKey)
 	keypair.PrivateKey = new(PrivateKey)
@@ -54,32 +38,63 @@ func GenerateKeyPair(bits uint) (keypair *KeyPair, err error) {
 		return
 	}
 
-	//Divide by 16 so the multiple of p and q is parameter "bits" bits large.
-	bytes := bits / 16
+	for limit := 0; limit < 1000; limit++ {
+		var tempKey *rsa.PrivateKey
+		tempKey, err = rsa.GenerateKey(rand.Reader, bits)
 
-	p, _ := GenerateBigPrime(bytes)
-	q, err := GenerateBigPrime(bytes)
-	if err != nil {
+		if err != nil {
+			return
+		}
+
+		if len(tempKey.Primes) != 2 {
+			err = errors.New("RSA package generated a weird set of primes (i.e. not two)")
+			return
+		}
+
+		p := tempKey.Primes[0]
+		q := tempKey.Primes[1]
+
+		if p.Cmp(q) == 0 {
+			err = errors.New("RSA keypair factors were equal. This is really unlikely dependent on the bitsize and it appears something horrible has happened.")
+			return
+		}
+		if gcd := new(big.Int).GCD(nil, nil, p, q); gcd.Cmp(big.NewInt(1)) != 0 {
+			err = errors.New("RSA primes were not relatively prime!")
+			return
+		}
+
+		modulus := new(big.Int).Mul(p, q)
+
+		publicExp := big.NewInt(3)
+		//publicExp := big.NewInt(65537)
+
+		//totient = (p-1) * (q-1)
+		totient := new(big.Int)
+		totient.Sub(p, big.NewInt(1))
+		totient.Mul(totient, new(big.Int).Sub(q, big.NewInt(1)))
+
+		if gcd := new(big.Int).GCD(nil, nil, publicExp, totient); gcd.Cmp(big.NewInt(1)) != 0 {
+			continue
+		}
+
+		privateExp := new(big.Int).ModInverse(publicExp, totient)
+		keypair.PublicKey.Modulus = modulus
+		keypair.PrivateKey.Modulus = modulus
+		keypair.PublicKey.PublicExp = publicExp
+		keypair.PrivateKey.PrivateExp = privateExp
 		return
 	}
-	if p.Cmp(q) == 0 {
-		err = errors.New("RSA keypair factors were equal. This is really unlikely dependent on the bitsize and it appears something horrible has happened.")
-		return
-	}
-	modulus := new(big.Int).Mul(p, q)
-	publicExp := big.NewInt(3)
-	//publicExp := big.NewInt(65537)
-
-	//totient = (p-1) * (q-1)
-	totient := new(big.Int)
-	totient.Sub(p, big.NewInt(1))
-	totient.Mul(totient, new(big.Int).Sub(q, big.NewInt(1)))
-
-	privateExp := new(big.Int).ModInverse(totient, modulus)
-
-	keypair.PublicKey.Modulus = modulus
-	keypair.PrivateKey.Modulus = modulus
-	keypair.PublicKey.PublicExp = publicExp
-	keypair.PrivateKey.PrivateExp = privateExp
+	err = errors.New("Failed to generate a within the limit!")
 	return
+
+}
+
+func Encrypt(message []byte, publicKey *PublicKey) *big.Int {
+	messageNum := new(big.Int).SetBytes(message)
+	ciphertext := new(big.Int).Exp(messageNum, publicKey.PublicExp, publicKey.Modulus)
+	return ciphertext
+}
+
+func Decrypt(ciphertext *big.Int, privateKey *PrivateKey) []byte {
+	return new(big.Int).Exp(ciphertext, privateKey.PrivateExp, privateKey.Modulus).Bytes()
 }
--- rsa/rsa_test.go
+++ rsa/rsa_test.go
@@ -4,6 +4,20 @@ import (
 	"testing"
 )
 
+//func TestPrimeGeneration(t *testing.T) {
+//	for i := 0; i < 100; i++ {
+//		p, err := GenerateBigPrime(512 / 8)
+//		if err != nil {
+//			t.Errorf("Failed to generate big prime!\n")
+//			return
+//		}
+//		if !p.ProbablyPrime(25) {
+//			t.Errorf("GenerateBigPrime returned a non-prime number\n")
+//			return
+//		}
+//	}
+//}
+
 func TestImproperKeypairGen(t *testing.T) {
 	_, err := GenerateKeyPair(0)
 	if err == nil {
@@ -20,7 +34,7 @@ func TestKeypairGen(t *testing.T) {
 	_, err := GenerateKeyPair(1024)
 
 	if err != nil {
-		t.Errorf("Failed to generated keypair with error:\n%v\n", err)
+		t.Errorf("Failed to generate keypair with error:\n%v\n", err)
 	}
 }
 
@@ -31,4 +45,22 @@ func TestKeypairGenReliablility(t *testing.T) {
 			t.Errorf("Failed to complete 100 error-free iterations of keygen. Failed at iteration %v.\n", i)
 		}
 	}
+}
+
+func TestEncryptionAndDecryption(t *testing.T) {
+
+	keypair, err := GenerateKeyPair(1024)
+	if err != nil {
+		t.Errorf("Failed to generate keypair with error:\n%v\n", err)
+		return
+	}
+	message := []byte{42}
+	ciphertext := Encrypt(message, keypair.PublicKey)
+	result := Decrypt(ciphertext, keypair.PrivateKey)
+
+	if message[0] != result[0] || len(result) != 1 {
+		t.Errorf("Failed to properly decrypt encrypted data.")
+		return
+	}
+
 }
